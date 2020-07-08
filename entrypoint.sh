f689b053c5cfac44b24905e320eca859f29cd7e8
Han Qiao <qiao@basis-ai.com>
2020-07-08 17:37:26 +0800

    Add server file

---
+++ entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -eu
+
+# Validate arguments
+WORKERS="${WORKERS:-2}"
+if [ "$WORKERS" -gt 0 ]; then
+    echo "Starting gunicorn with $WORKERS workers"
+else
+    echo "Invalid argument: WORKERS=$WORKERS"
+    exit 1
+fi
+
+EXTRA_OPTS=""
+if [ "$WORKERS" -gt 1 ]; then
+    export prometheus_multiproc_dir=/tmp
+    EXTRA_OPTS="--config gunicorn_config.py"
+fi
+
+exec gunicorn serve_http:app \
+    "$EXTRA_OPTS" \
+    --bind=":${BEDROCK_SERVER_PORT:-8080}" \
+    --worker-class=gthread \
+    --workers="$WORKERS" \
+    --timeout=300 \
+    --preload
---
+++ serve_http.py
@@ -0,0 +1,55 @@
+from importlib import import_module
+from os import getenv
+
+from bedrock_client.bedrock.metrics.service import ModelMonitoringService
+from flask import Flask, Response, current_app, request
+
+serve = import_module(getenv("BEDROCK_SERVER", "serve"))
+model = serve.Model()
+
+app = Flask(__name__)
+
+
+@app.before_first_request
+def init_background_threads():
+    """Global objects with daemon threads will be stopped by gunicorn --preload flag.
+    So instantiate the model monitoring service here instead.
+    """
+    current_app.monitor = ModelMonitoringService()
+
+
+@app.route("/", methods=["POST"])
+def predict():
+    # User code to load features
+    features = (
+        serve.pre_process(request.data)
+        if hasattr(serve, "pre_process")
+        else [float(x) for x in request.json]
+    )
+
+    # Compute the probability of the first class (True)
+    score = model.predict(features)
+
+    if hasattr(serve, "post_process"):
+        score = serve.post_process(score)
+
+    pid = current_app.monitor.log_prediction(
+        request_body=request.json, features=features, output=score,
+    )
+
+    return {"result": score, "prediction_id": pid}
+
+
+@app.route("/metrics", methods=["GET"])
+def get_metrics():
+    """Returns real time feature values recorded by Prometheus
+    """
+    body, content_type = current_app.monitor.export_http(
+        params=request.args.to_dict(flat=False), headers=request.headers,
+    )
+    return Response(body, content_type=content_type)
+
+
+if __name__ == "__main__":
+    """Starts the Http server"""
+    app.run()
