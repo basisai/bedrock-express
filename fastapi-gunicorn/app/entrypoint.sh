2b21156c26af077b0c508fc50460632f877b2be4
Lim Swee Kiat <limsweekiat@gmail.com>
2020-07-15 10:52:28 +0800

    Remove --preload from gunicorn command

--- fastapi-gunicorn/app/entrypoint.sh
+++ fastapi-gunicorn/app/entrypoint.sh
@@ -25,5 +25,4 @@ exec gunicorn serve_http:app \
     --bind=":${BEDROCK_SERVER_PORT:-8080}" \
     --worker-class=uvicorn.workers.UvicornWorker \
     --workers="$WORKERS" \
-    --timeout=300 \
-    --preload
+    --timeout=300
