ee9cac2b3f6919bea0291fbbe5b3b89da4289e16
Han Qiao <qiao@basis-ai.com>
2020-07-28 15:35:43 +0800

    Add inference histogram for continuous variable

--- tests/tf-vision/model-server/serve.py
+++ tests/tf-vision/model-server/serve.py
@@ -2,13 +2,12 @@
 Script for serving.
 """
 from PIL import Image
-import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
 
 
 def pre_process(http_body, files):
-    return np.array(Image.open(files["image"]).convert('RGB').resize((299, 299)))
+    return [Image.open(files["image"]).convert('RGB').resize((299, 299))]
 
 
 class Model:
@@ -19,8 +18,12 @@ class Model:
         self.model.build([None, 299, 299, 3])
 
     def predict(self, features):
-        """
-        features: Array-like of shape (height, width, 3)
+        """Makes an inference using the model.
+
+        :param features: An iterable of array-like of shape (height, width, 3)
+        :type features: Iterable
+        :return: Class label from ImageNet
+        :rtype: int
         """
         img = tf.image.convert_image_dtype(features, tf.float32)[tf.newaxis, ...]
         logits = self.model(img)
