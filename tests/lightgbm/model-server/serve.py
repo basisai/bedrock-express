ba7a6a26408a34b37679704f475aedf54c7871da
Han Qiao <qiao@basis-ai.com>
2020-08-24 01:16:24 +0800

    Make paths dynamic

--- tests/lightgbm/model-server/serve.py
+++ tests/lightgbm/model-server/serve.py
@@ -1,12 +1,12 @@
 import pickle
-from typing import List
+from typing import List, Optional
 
 from bedrock_client.bedrock.model import BaseModel
 
 
 class Model(BaseModel):
-    def __init__(self, path: str = "/artefact/model.pkl"):
-        with open(path, "rb") as f:
+    def __init__(self, path: Optional[str] = None):
+        with open(path or "/artefact/model.pkl", "rb") as f:
             self.model = pickle.load(f)
 
     def predict(self, features: List[List[float]]) -> List[float]:
--- tests/lightgbm/model-server/test_serve.py
+++ tests/lightgbm/model-server/test_serve.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from unittest import TestCase
 
 from serve import Model
@@ -5,7 +6,8 @@ from serve import Model
 
 class TestModelServer(TestCase):
     def test_validate(self):
-        m = Model()
+        p = Path(__file__).parent.parent / "artefact" / "model.pkl"
+        m = Model(path=p)
         result = m.validate(http_body=str([[i for i in range(66)]]))
         self.assertIn("result", result)
         self.assertIn("prediction_id", result)
