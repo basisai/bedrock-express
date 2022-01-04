9cc3a63480a00e07080e01220c179c8258db5d38
Han Qiao <qiao.han@aicadium.ai>
2022-01-04 12:54:14 +0800

    Update lightgbm model to v3

--- tests/lightgbm/model-server/test_serve.py
+++ tests/lightgbm/model-server/test_serve.py
@@ -6,10 +6,11 @@ from serve import Model
 
 class TestModelServer(TestCase):
     def test_validate(self):
-        p = Path(__file__).parent.parent / "artefact" / "model.pkl"
-        m = Model(path=p)
-        result = m.validate(http_body=str([[i for i in range(66)]]))
+        path = Path(__file__).parent.parent / "artefact" / "model.pkl"
+        m = Model(path=path)
+        features = list(range(m.model.n_features_))
+        result = m.validate(http_body=str([features]))
         self.assertIn("result", result)
         self.assertIn("prediction_id", result)
         self.assertEqual(len(result["prediction_id"].split("/")), 3)
-        self.assertEqual(result["result"], [0.9793770021409441])
+        self.assertEqual(result["result"], [0.9489560209860071])
